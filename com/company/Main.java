78e5d0d7ac49f164e2e25a9272942c051b8e1981
hkod19 <hadiyakodvavi@gmail.com>
2018-06-30 17:12:26 -0400

    change

--- com/company/Main.java
+++ com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("hello hadiya");
+        System.out.println("hello SIRAJ");
         System.out.println("this is confusing");
 
         try {
